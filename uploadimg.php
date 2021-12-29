f003e15fde7a608ff6fb7d983ed441f3d1511bb4
TotallyDone <54904876+TotallyDone@users.noreply.github.com>
2021-12-29 01:13:46 +0100

    Add files via upload

--- uploadimg.php
+++ uploadimg.php
@@ -19,24 +19,11 @@ include_once "assets/includes/head.inc.php";
     </div>
 
     <?php 
-    $id = $_SESSION['userProfileID'];
-
-    $sqlImg = "SELECT * FROM profileimg WHERE userProfileID = " . $id;
-    $resultimg = mysqli_query($conn , $sqlImg);
-    while ($row = mysqli_fetch_array($resultimg)){
-        
-
-        if($row['statusimg'] == 0){
-            echo "<div class='center'><img class='profilepic-upload' src='assets/img/profiledefault.jpg' alt='profile pic'> </div>";
-            
-            echo"<form action='assets/includes/upload.php' enctype='multipart/form-data' method='post'>
-            <input type='file' name='userImage' accept='image/png, image/jpeg' required><br><br>
-            <button  name='submit' type='submit'>upload profilepic</button>
-            </form>";
-        }else{
-            echo "<div class='center'><img class='profilepic' src='assets/img/profile".$id.".jpg' alt='profile pic". $id ."'></div>";
-        }
-    }
+    
+    echo"<div class='center'><form action='assets/includes/upload.php' enctype='multipart/form-data' method='post'>
+    <input type='file' name='userImage' accept='image/png, image/jpeg' required><br><br>
+    <button  name='submit' type='submit'>upload profilepic</button>
+    </form></div>";
     ?>
     
 </body>
